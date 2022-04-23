24997ae378c61166a234e4e5264bfa6c218a1b9c
Mrhdavoodi <mr.hdavoodi@gmail.com>
2022-04-23 17:57:58 +0200

    new directory structure and for section

---
+++ chapter2/for-statement/for-challenge1.py
@@ -0,0 +1,9 @@
+# function returns a sequence of numbers,
+# starting from 0 by default, and increments 
+# by 1 (by default), and stops before a specified number.
+print('Here are the odds:')
+for i in range(1, 10, 2): 
+    print( i )
+print('Here are the evens:')
+for i in range(0, 10, 2): 
+    print( i )
---
+++ chapter2/for-statement/for-challenge2-range.py
@@ -0,0 +1,6 @@
+# Printing the number (between 15 to 45) 
+# that are devideale by 3
+x = range( 15, 45 )
+for i in x:
+    if(i % 3 == 0):
+        print(i)
