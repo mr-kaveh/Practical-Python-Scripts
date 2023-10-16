b3cc7426e81f855b504ad244ccd2a8ab4f1c9d45
mr-kaveh <mr.hdavoodi@gmail.com>
2023-10-16 20:04:42 +0200

    Create for-challenge8-pass.py

---
+++ chapter2/for-statement/for-challenge8-pass.py
@@ -0,0 +1,18 @@
+# the pass statement is a null operation, meaning it does nothing. 
+# It's often used as a placeholder when a statement is syntactically 
+# required but you don't want to execute any code.
+def example_function():
+    # TODO: Implement this function later
+    pass
+
+for i in range(5):
+    if i % 2 == 0:
+        print(f"{i} is even.")
+    else:
+        # Placeholder for handling odd numbers
+        pass
+
+class EmptyClass:
+    pass
+
+# The pass statement can also be used in other contexts where a statement is required.
